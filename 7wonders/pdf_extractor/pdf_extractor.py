f407901a737180b84c02139cdc1b667e4f17339e
Romain Thibault <romain.thibault@mclaren.com>
2019-06-03 15:33:45 +0100

    extracted 7wonders card from pdf; guilds are cropped though, because wider => run specific extractor for this

--- 7wonders/pdf_extractor/pdf_extractor.py
+++ 7wonders/pdf_extractor/pdf_extractor.py
@@ -7,6 +7,7 @@ try:
 	# standard imports
 	import os
 	import subprocess
+	import numpy as np
 	
 	# custom imports
 	import Misc
@@ -15,14 +16,15 @@ try:
 	ThisFolder = os.path.dirname(os.path.realpath(__file__))	
 	TopFolder = os.path.dirname(ThisFolder)	
 	
+	# change working directory
+	os.chdir(TopFolder)
+	
 	# paths
 	PdfSrcPath = r'raw\7Wonders-CardsList-EN.pdf'
 	if not os.path.exists('proc'):
 		os.makedirs('proc')
 	HtmlDstPath = PdfSrcPath.replace('raw', 'proc').replace("pdf", "html")
 	
-	# change working directory
-	os.chdir(TopFolder)
 	
 	# extract words with associated bounding box from pdf
 	subprocess.check_output = handle_retval_and_log(subprocess.check_output) # logging decorator
@@ -35,24 +37,36 @@ try:
 			# ).decode()
 		# )
 	# subprocess.check_output([r"poppler-0.68.0\bin\pdftohtml.exe", "-xml", PdfSrcPath, r'proc\7wonders_card.xml'])
+	# subprocess.check_output([r"poppler-0.68.0\bin\pdftocairo.exe", "-jpeg", "-r", "900", PdfSrcPath, r'proc\7wonders_cards.jpg'])
 	# subprocess.check_output([r'poppler-0.68.0\bin\pdftocairo.exe -png -f 1 -l 1 -x 172 -y 484 -W 196 -H 83 raw\7Wonders-CardsList-EN.pdf'])
-	subprocess.check_output([
-		r'poppler-0.68.0\bin\pdftocairo.exe',
-		'-png',
-		'-f',
-		'1',
-		'-l',
-		'1',
-		'-x',
-		'406',
-		'-y',
-		'484',
-		'-W',
-		'196',
-		'-H',
-		'83',
-		r'raw\7Wonders-CardsList-EN.pdf', 
-	])
+	
+	# 150 dpi
+	# x_ar = np.linspace(172, 872, 4)
+	# y_ar = np.linspace(484, 1305, 10)
+	
+	# 900 dpi
+	x_ar = np.linspace(1035, 5234, 4)
+	y_ar = np.linspace(2904, 7831, 10)
+	for x in x_ar:
+		for y in y_ar:
+			x_str = str(int(round(x)))
+			y_str = str(int(round(y)))
+			subprocess.check_output([
+				r'poppler-0.68.0\bin\pdftocairo.exe',
+				'-jpeg',
+				'-r',
+				'900',
+				'-x',
+				x_str,
+				'-y',
+				y_str,
+				'-W',
+				'1171',
+				'-H',
+				'498',
+				r'raw\7Wonders-CardsList-EN.pdf',
+				r'proc\7wonders-card-{}-{}'.format(x_str, y_str),
+			])
 
 except:
 
